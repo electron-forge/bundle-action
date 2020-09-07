7cdfb3e9c8980ee72276705f19cff7ef00dc1cf6
Mark Lee <electronjs@lazymalevolence.com>
2020-09-06 21:29:46 -0700

    fix: only run the import script on macOS

--- scripts/import-macos-cert.sh
+++ scripts/import-macos-cert.sh
@@ -6,25 +6,27 @@
 # * `MACOS_CERT_PASSWORD`: password to unlock the certificate so it can be imported
 #
 
-KEY_CHAIN=build.keychain
-KEY_CHAIN_USER=actions
-MACOS_CERT_P12_FILE=certificate.p12
+if test "$(uname -s)" = "Darwin"; then
+  KEY_CHAIN=build.keychain
+  KEY_CHAIN_USER=actions
+  MACOS_CERT_P12_FILE=certificate.p12
 
-# Recreate the certificate from the secure environment variable
-echo $MACOS_CERT_P12 | base64 --decode > $MACOS_CERT_P12_FILE
+  # Recreate the certificate from the secure environment variable
+  echo $MACOS_CERT_P12 | base64 --decode > $MACOS_CERT_P12_FILE
 
-# Create a keychain
-security create-keychain -p $KEY_CHAIN_USER $KEY_CHAIN
+  # Create a keychain
+  security create-keychain -p $KEY_CHAIN_USER $KEY_CHAIN
 
-# Make the keychain the default so identities are found
-security default-keychain -s $KEY_CHAIN
+  # Make the keychain the default so identities are found
+  security default-keychain -s $KEY_CHAIN
 
-# Unlock the keychain
-security unlock-keychain -p $KEY_CHAIN_USER $KEY_CHAIN
+  # Unlock the keychain
+  security unlock-keychain -p $KEY_CHAIN_USER $KEY_CHAIN
 
-security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERT_PASSWORD -T /usr/bin/codesign;
+  security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERT_PASSWORD -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple: -s -k $KEY_CHAIN_USER $KEY_CHAIN
+  security set-key-partition-list -S apple-tool:,apple: -s -k $KEY_CHAIN_USER $KEY_CHAIN
 
-# Remove cert created from this script
-rm $MACOS_CERT_P12_FILE"
+  # Remove cert created from this script
+  rm "$MACOS_CERT_P12_FILE"
+fi
